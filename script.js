72149d29a1e2d0215698e124870ad6ff87c85cd4
gabriellialbano <gabriellicristinaalbano@gmail.com>
2024-06-27 13:10:06 -0300

    a

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 // Defina a URL do servidor que você deseja acessar
 var url = "https://gabriellialbano.github.io/contato.html";
 
-// Defina os dados que serão enviados para o servidor
-var dados = { nome: "Gabrielli", idade: 18 };
-
-// Faça a requisição POST utilizando a API Fetch
-fetch(url, {
-  method: "POST",
-  body: JSON.stringify(dados),
-  headers: {
-    "Content-Type": "application/json"
-  }
-})
+// Faça a requisição GET utilizando a API Fetch
+fetch(url)
  .then(response => response.text())
  .then(resposta => console.log(resposta));
